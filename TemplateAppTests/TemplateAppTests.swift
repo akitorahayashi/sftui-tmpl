3410a600e40475f17eceee26267fdd9cf54175b4
atrh95 <atora.has75@gmail.com>
2025-06-25 22:46:15 +0900

    Testファイル構成のテンプレートを作成

---
+++ TemplateAppTests/TemplateAppTests.swift
@@ -0,0 +1,8 @@
+import XCTest
+@testable import TemplateApp
+
+final class TemplateAppTests: XCTestCase {
+    func testExample() {
+        XCTAssertTrue(true)
+    }
+} 
---
+++ TemplateAppUITests/TemplateAppUITests.swift
@@ -0,0 +1,11 @@
+import XCTest
+
+final class TemplateAppUITests: XCTestCase {
+
+    func testExample() throws {
+        let app = XCUIApplication()
+        app.launch()
+        
+        XCTAssertTrue(app.wait(for: .runningForeground, timeout: 5))
+    }
+} 
